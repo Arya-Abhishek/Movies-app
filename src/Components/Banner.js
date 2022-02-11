8cd84bdfe1189e9bbbf242a65190285cd5d92a95
Abhishek <arya2001abhishek@gmail.com>
2022-02-11 18:17:47 +0530

    add favourites page UI and table

--- src/Components/Banner.js
+++ src/Components/Banner.js
@@ -6,21 +6,21 @@ export default class Banner extends Component {
     let movie = movies.results[1]
     console.log(movie);
     return (
-        <>
-        {   movie == '' ? 
-        <div className="spinner-border text-primary" role="status">
-        <span className="visually-hidden">Loading...</span>
-        </div> :
-            <div className="card banner-card">
-                <img src={`https://image.tmdb.org/t/p/original/${movie.backdrop_path}`} className="card-img-top banner-img" alt="..." />
-                {/* <div className="card-body"> */}
-                    <h1 className="card-title banner-title">{movie.original_title}</h1>
-                    <p className="card-text banner-text">{movie.overview}</p>
-                    {/* <a href="#" className="btn btn-primary">Go somewhere</a> */}
-                {/* </div> */}
-            </div>
+      <>
+        {movie == '' ?
+          <div className="spinner-border text-primary" role="status">
+            <span className="visually-hidden">Loading...</span>
+          </div> :
+          <div className="card banner-card">
+            <img src={`https://image.tmdb.org/t/p/original/${movie.backdrop_path}`} className="card-img-top banner-img" alt="..." />
+            {/* <div className="card-body"> */}
+            <h1 className="card-title banner-title">{movie.original_title}</h1>
+            <p className="card-text banner-text">{movie.overview}</p>
+            {/* <a href="#" className="btn btn-primary">Go somewhere</a> */}
+            {/* </div> */}
+          </div>
         }
-        </>
+      </>
     )
   }
 }
